d7cfe6ce52c120186955cb57934bf36876367cbf
Cristian <cristiandepena@hotmail.com>
2020-02-19 19:43:54 -0400

    Adding carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as M from 'materialize-css';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CocinaApp';
+
+  OnInit() {
+    document.addEventListener('DOMContentLoaded', function() {
+      const elems = document.querySelectorAll('.carousel');
+      const instances = M.Carousel.init(elems);
+    });
+  }
 }
